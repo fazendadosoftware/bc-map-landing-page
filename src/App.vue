88dbd12416dcd474c07db7c2086b220e57d228aa
Paulo dos Santos <paulo@motovalor.com>
2021-03-04 11:49:00 -0300

    current state

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template>
-  <div class="relative w-screen">
-    <business-capability-map />
-  </div>
+  <div class="bg-gray-200 sticky top-0 h-40 border-b"/>
+  <business-capability-map />
+  <div>casjsaoi</div>
+  <div>fjwoejfoiwejofejwio</div>
+
 </template>
 
 <script>
