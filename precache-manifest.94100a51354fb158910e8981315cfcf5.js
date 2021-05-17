98bd765c8cb9b9baefa7ed13d1ed39dd0797be62
Paulo dos Santos <paulo@motovalor.com>
2021-05-17 17:58:55 -0300

    deploy

---
+++ precache-manifest.94100a51354fb158910e8981315cfcf5.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c51774b84cc552ddb5da",
+    "url": "/bc-map-landing-page/css/app.6f42a154.css"
+  },
+  {
+    "revision": "a005d0e079c4468f4ead56a6091c66a3",
+    "url": "/bc-map-landing-page/img/best-practices-1.a005d0e0.png"
+  },
+  {
+    "revision": "2a26515377863cd507310b87e0c45795",
+    "url": "/bc-map-landing-page/img/best-practices-2.2a265153.png"
+  },
+  {
+    "revision": "23954d3dfd2e461a9783600942f0c3c1",
+    "url": "/bc-map-landing-page/img/best-practices-3.23954d3d.png"
+  },
+  {
+    "revision": "a21bfbdb76279619cadbfef6d36f0a6c",
+    "url": "/bc-map-landing-page/img/best-practices-7.a21bfbdb.png"
+  },
+  {
+    "revision": "2aa0aaf3cf9828d972e50208378bacea",
+    "url": "/bc-map-landing-page/img/best-practices-8.2aa0aaf3.png"
+  },
+  {
+    "revision": "e42c2c679fc55318f1423c99545e0425",
+    "url": "/bc-map-landing-page/img/leanix-logo.e42c2c67.png"
+  },
+  {
+    "revision": "4f9430839cd8620923341f0329d70e3a",
+    "url": "/bc-map-landing-page/index.html"
+  },
+  {
+    "revision": "c51774b84cc552ddb5da",
+    "url": "/bc-map-landing-page/js/app.32b6673c.js"
+  },
+  {
+    "revision": "bafa2372ba1563ad22f3",
+    "url": "/bc-map-landing-page/js/chunk-2d0b1f6a.872a8ce2.js"
+  },
+  {
+    "revision": "be22924b775cf579508a",
+    "url": "/bc-map-landing-page/js/chunk-2d216214.49bf1fba.js"
+  },
+  {
+    "revision": "ac506c3460aa92a2c5b5",
+    "url": "/bc-map-landing-page/js/chunk-2d216257.256ee085.js"
+  },
+  {
+    "revision": "032cd68fe5f082210e2e",
+    "url": "/bc-map-landing-page/js/chunk-4ce4d5a9.689280b3.js"
+  },
+  {
+    "revision": "755a83eb964f04d4c9a5",
+    "url": "/bc-map-landing-page/js/chunk-4f99ac38.ea1c256d.js"
+  },
+  {
+    "revision": "2a178bdc1282584c077a",
+    "url": "/bc-map-landing-page/js/chunk-9879326a.d84351fa.js"
+  },
+  {
+    "revision": "86b5314c9aedc061579e",
+    "url": "/bc-map-landing-page/js/chunk-vendors.3951e34c.js"
+  },
+  {
+    "revision": "00d694a6eadc6ba85b8bf95b1a9ddeaf",
+    "url": "/bc-map-landing-page/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/bc-map-landing-page/robots.txt"
+  }
+]);
